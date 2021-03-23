9682979bc95d4da1f516239e76732a89a93adc91
Lian <liulian1004@hotmail.com>
2021-03-22 22:07:57 -0700

    go project re-delopy

---
+++ go.mod
@@ -0,0 +1,3 @@
+module balanceSheet
+
+go 1.16
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+// import (
+// 	"balanceSheet/function/utils"
+// 	"fmt"
+// )
 import (
-	"./src/utils"
+	"balanceSheet/utils"
+	// "balanceSheet/utils"
+	"fmt"
+	// "utils"
 )
 
 func main(){
 	//step1, create obj
 	//call method
 	utils.NewFamilyAccount().DisplayMenu();
+	fmt.Println("hello world")
+
 }
